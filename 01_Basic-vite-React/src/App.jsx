806e1c6f79fe23a413391f431ca7b236dd5df661
Deep Kansara <deepkansara1212@gmail.com>
2024-09-24 11:17:55 +0530

    React with vite

--- 01_Basic-vite-React/src/App.jsx
+++ 01_Basic-vite-React/src/App.jsx
@@ -2,10 +2,12 @@ import Chai from './Chai.jsx'
 
 function App() {
 
+  const username = "HC"
   return (
     <>
       <Chai/>
-      <h1>Chai Aur React</h1> 
+      {/* This {username} is known as "Evaluated Expression" i.e. we have only put the evaluated output of the JS */}
+      <h1>Chai Aur React | {username}</h1> 
     </>
   )
 } 
--- 01_Basic-vite-React/src/main.jsx
+++ 01_Basic-vite-React/src/main.jsx
@@ -1,9 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <>
+      <h1>Custom App | DK</h1> 
+    </>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// } 
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+
+// Pre-defined format of React
+const ReactElement = React.createElement(
+  // Expected Parameter: tag, object, text, variables
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google',
+  anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+
+  // <App /> 
+  ReactElement 
+
 )
