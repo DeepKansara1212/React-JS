01357670a2799310d4da9d058816bb3c05f56ce2
Deep Kansara <deepkansara1212@gmail.com>
2024-09-25 12:54:44 +0530

    Tailwind & Props

---
+++ 03_Tailwind-Props/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  /*
+  let myObj = {
+    username: "Deep",
+    age: 22
+  }
+
+  let newArr = [1,2,3,4]
+  */
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1> 
+
+      {/* This is how we can pass the props */}
+      {/* <Card channel="chai aur code" someObj={myObj} someArr={newArr} /> */}
+      <Card username="Deep" btnText="Click Me" /> 
+      <Card username="Manan" /> 
+    </>
+  )
+};
+
+export default App
